0c9c29e21b85e883980d0abcf3206718e0e249f5
Ben Ramirez <tarmi13@gmail.com>
2024-09-05 17:45:40 -0600

    rename

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from promotions.promotions.spiders.spider1 import Spider1  #
 query = "quarry jeans"
 results = DDGS().text(query, max_results=5)
 urls = [result['href'] for result in results]
-
 process = CrawlerProcess(get_project_settings())
-
 process.crawl(Spider1, keyword="look", urls=urls)
-
 process.start()
